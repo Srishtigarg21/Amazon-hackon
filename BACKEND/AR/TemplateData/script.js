e1aa3a5456a95932e96769c4a13de47cb146a5a5
Srishtigarg21 <110321041+Srishtigarg21@users.noreply.github.com>
2022-07-31 10:58:58 +0530

    Add files via upload

---
+++ BACKEND/AR/TemplateData/script.js
@@ -0,0 +1,38 @@
+let mediaRecorder
+let recordedBlobs
+const errorMsgElement = document.querySelector('span#errorMsg');
+const recordedVideo = document.querySelector('video#recorded');
+const recordButton = document.querySelector('button#record');
+const playButton = document.querySelector('button#play');
+const downloadButton = document.querySelector('button#download');
+
+document.querySelector('button#start').addEventListener('click', async () => {
+    const hasEchoCancellation = document.querySelector('#echoCancellation').checked;
+    const constraints = {
+      audio: {
+        echoCancellation: {exact: hasEchoCancellation}
+      },
+      video: {
+        width: 1280, height: 720
+      }
+    };
+    console.log('Using media constraints:', constraints);
+    await init(constraints);
+  });
+  async function init(constraints) {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      handleSuccess(stream);
+    } catch (e) {
+      console.error('navigator.getUserMedia error:', e);
+      errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+    }
+  }
+  function handleSuccess(stream) {
+    recordButton.disabled = false;
+    console.log('getUserMedia() got stream:', stream);
+    window.stream = stream;
+  
+    const gumVideo = document.querySelector('video#gum');
+    gumVideo.srcObject = stream;
+  }
